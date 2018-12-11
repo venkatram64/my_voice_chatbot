aef52e289ee1944da277ff67b07de73336f7f574
venkatram64 <venkat.veerareddy@hotmail.com>
2018-12-11 10:22:01 +0530

    init commit

--- my_chatter/my_chat.py
+++ my_chatter/my_chat.py
@@ -3,7 +3,6 @@ from chatterbot import ChatBot
 import os
 import pyttsx3 #speech library for text to speech conversation
 
-#chatterbot, pyttsx3, pypiwin32
 
 bot = ChatBot('MyBot')
 #conv = open('./data/chats.txt', 'r').readlines()
@@ -19,21 +18,27 @@ my_bot_v = my_bot.getProperty('voices')
 my_bot.setProperty('voice', my_bot_v[1].id)
 my_bot.setProperty('rate', 150)
 my_bot.setProperty('volume', 5)
+
 for _file in os.listdir('data'):
     chats = open('data/' + _file, 'r').readlines()
 
 bot.train(chats)
 
+my_bot.say("Welcome to chat!!!")
+my_bot.runAndWait()
+
 while True:
     request = input('You: ')
 
     if request.strip() == 'Bye':
+        my_bot.say("See you!!!")
+        my_bot.runAndWait()
         print('Bot: '+ "Good Bye!")
         break
 
     response = bot.get_response(request)
 
-    if response.confidence > 0.75:
+    if response.confidence > 0.6:
         my_bot.say(response)
         print('Bot: ', response)
         my_bot.runAndWait()
